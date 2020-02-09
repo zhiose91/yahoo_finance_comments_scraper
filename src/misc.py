4028f95bda3e4f3753c8040432b9a52e92fe122a
Zhiheng Dong <zhiheng.dong@baruchmail.cuny.edu>
2020-02-09 18:18:58 -0500

    Day 2: Need to get text within links or react-text

--- src/misc.py
+++ src/misc.py
@@ -1,5 +1,5 @@
 import json
 
-def json_link_reader(file_name):
+def json_reader(file_name):
     with open(file_name, "r") as file:
         return json.load(file) 
--- web_fetcher.py
+++ web_fetcher.py
@@ -1,4 +1,4 @@
-from src.web_utils import json_link_reader
+from src.misc import json_reader
 from src.chrome_utils import download_driver
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -8,6 +8,8 @@ import time
 
 
 def yahoo_finance_top_comments_fetcher(*, web_links: list):
+    xp_elems = json_reader(file_name="xp_elems.json")
+
     options = webdriver.ChromeOptions()
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--ignore-ssl-errors')
@@ -15,32 +17,37 @@ def yahoo_finance_top_comments_fetcher(*, web_links: list):
         driver = webdriver.Chrome(chrome_options=options)
         driver.get(web_link)
 
-        # Top Reactions
-        top_react_xp = "//span[text()[contains(.,'Top Reactions')]]"
-        # Newest Reactions
-        newest_xp = "//span[text()[contains(.,'Newest Reactions')]]"
-        # Title
-        title_xp = "//h1[@class='D(ib) Fz(18px)']"
-        index_xp = "//span[@class='Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)']"
-        mov_xp = "//span[@class='Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($dataRed)']"
-
         # comments_xp = "//div[text()[contains(., 'react-text')]]"
+        comment_timestamp_xp = "//span[@class='C($c-fuji-grey-g) Fz(12px)']//span"
         comments_xp = "//div[@class='Wow(bw)']//div[@class='C($c-fuji-grey-l) Mb(2px) Fz(14px) Lh(20px) Pend(8px)']"
+        # comments_xp = "//div[@class='Wow(bw)']//div[@class='canvass-see-more']/div/div"
 
         try:
             element = WebDriverWait(driver, 20).until(
-                EC.presence_of_element_located((By.XPATH, top_react_xp))
+                EC.presence_of_element_located((By.XPATH, xp_elems["top_react"]))
             )
-            print("="*30)
-            print(f"\nTitle: {driver.find_element_by_xpath(title_xp).text}")
-            print(f"Index: {driver.find_element_by_xpath(index_xp).text}")
-            print(f"Movement: {driver.find_element_by_xpath(mov_xp).text}")
-            print(f"\nFetching Newest Comments...\n")
-            driver.find_element_by_xpath(top_react_xp).click()
-            driver.find_element_by_xpath(newest_xp).click()
-            time.sleep(10)
-            print(f"{driver.find_element_by_xpath(comments_xp).text}")
 
+            print("="*30)
+            print(xp_elems["title"])
+            print(f'\nTitle: {driver.find_element_by_xpath(xp_elems["title"]).text}')
+            print(f'Index: {driver.find_element_by_xpath(xp_elems["index"]).text}')
+            print(f'Movement: {driver.find_element_by_xpath(xp_elems["movement"]).text}')
+            print('\nFetching Newest Comments...\n')
+
+            driver.find_element_by_xpath(xp_elems["top_react"]).click()
+            driver.find_element_by_xpath(xp_elems["newest"]).click()
+
+            time.sleep(10)
+            time_stamps = driver.find_elements_by_xpath(xp_elems["comment_timestamp"])
+            for time_stamp in time_stamps[:8]:
+                print(time_stamp.text)
+            print("\n")
+            comments = driver.find_elements_by_xpath(comments_xp)
+            for comment in comments[:8]:
+                print(comment.text, "\n")
+
+            # for time_stamp, comment in list(zip(time_stamps, comments))[0:10]:
+            #     print(f'{time_stamp.text}: {comment.text}\n')
         finally:
             print("="*30)
             time.sleep(3)
@@ -48,5 +55,5 @@ def yahoo_finance_top_comments_fetcher(*, web_links: list):
 
 if __name__ == '__main__':
     # download_driver()
-    web_links = json_link_reader(file_name="web_links.json")
+    web_links = json_reader(file_name="web_links.json")
     yahoo_finance_top_comments_fetcher(web_links=web_links)
