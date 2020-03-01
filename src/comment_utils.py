052719273696bae2e6224179a44361b785e3d095
Zhiheng Dong <zhiheng.dong@baruchmail.cuny.edu>
2020-03-01 00:28:52 -0500

    Added Soup function to Fetch user\timestamp comment within a day

---
+++ src/comment_utils.py
@@ -0,0 +1,10 @@
+# comment class
+# storing useful information about on column
+
+class Comment():
+    # Hello content
+    def __init__(self, user, time_stamp, comment_text):
+        self.user = user
+        self.time_stamp = time_stamp
+        self.comment_text = comment_text
+        
--- web_fetcher.py
+++ web_fetcher.py
@@ -4,7 +4,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
 import time
+import re
 
 
 def yahoo_finance_top_comments_fetcher(*, web_links: list):
@@ -13,19 +15,17 @@ def yahoo_finance_top_comments_fetcher(*, web_links: list):
     options = webdriver.ChromeOptions()
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--ignore-ssl-errors')
+    options.add_argument('headless')
+    options.add_argument("--log-level=3")
     for web_link in web_links:
         driver = webdriver.Chrome(chrome_options=options)
         driver.get(web_link)
-
-        # comments_xp = "//div[text()[contains(., 'react-text')]]"
-        comments_xp = "//div[@class='Wow(bw)']//div[@class='C($c-fuji-grey-l) Mb(2px) Fz(14px) Lh(20px) Pend(8px)']"
-        # comments_xp = "//div[@class='Wow(bw)']//div[@class='canvass-see-more']/div/div"
 
         try:
             WebDriverWait(driver, 20).until(EC.presence_of_element_located(
                                             (By.XPATH, xp_elems["top_react"])))
 
-            print("="*30)
+            print("="*60)
             print(f'\nTitle: {driver.find_element_by_xpath(xp_elems["title"]).text}')
             print(f'Index: {driver.find_element_by_xpath(xp_elems["index"]).text}')
             print(f'Movement: {driver.find_element_by_xpath(xp_elems["movement"]).text}')
@@ -33,20 +33,35 @@ def yahoo_finance_top_comments_fetcher(*, web_links: list):
 
             driver.find_element_by_xpath(xp_elems["top_react"]).click()
             driver.find_element_by_xpath(xp_elems["newest"]).click()
+            for i in range(1):
+                time.sleep(3)
+                driver.find_element_by_xpath(xp_elems["show_more"]).click()
 
+            # wait for 15 seconds before parsing through the file
             time.sleep(10)
-            # time_stamps = driver.find_elements_by_xpath(xp_elems["comment_timestamp"])
-            # for time_stamp in time_stamps[:8]:
-            #     print(time_stamp.text)
-            # print("\n")
-            comments = driver.find_elements_by_xpath(comments_xp)
-            for comment in comments[:10]:
-                print(comment.text, "\n")
 
-            # for time_stamp, comment in list(zip(time_stamps, comments))[0:10]:
-            #     print(f'{time_stamp.text}: {comment.text}\n')
+            # Now trying - get comment block element instead
+            # ------------------------------------------------------------------
+            comment_list_ele = driver.find_element_by_xpath(xp_elems["comment_list"])
+            comment_list_html = comment_list_ele.get_attribute('innerHTML')
+
+            comment_list_soup = BeautifulSoup(comment_list_html, 'html.parser')
+            comment_block_list = comment_list_soup.find_all("li", xp_elems["comment_block"])
+
+            for comment_block in comment_block_list:
+                user = comment_block.find("button", xp_elems["user_tag"])
+                time_stamp = comment_block.find("span", xp_elems["time_stamp"]).find("span")
+                # thumb_up_ct = comment_block.find("button", xp_elems["thumb_up_block"]) # .find("span", xp_elems["thumb_up_ct"])
+                # print(thumb_up_ct.text)
+                # thumb_down_ct = pass
+                if re.match(r".*(second|minute|hour).*", time_stamp.text):
+                    print(f"{user.text}[{time_stamp.text}]")
+
+            # ------------------------------------------------------------------
+
+
         finally:
-            print("="*30)
+            print("="*60)
             driver.quit()
 
 if __name__ == '__main__':
