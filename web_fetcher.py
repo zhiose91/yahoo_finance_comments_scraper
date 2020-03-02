0ac26e2936ed26378ec1b669c7aef054952f15fb
Zhiheng Dong <zhiheng.dong@baruchmail.cuny.edu>
2020-03-02 00:05:50 -0500

    Restructured main function in a more organize way, now everything is stored in a class object YF_comment_analyzer

--- web_fetcher.py
+++ web_fetcher.py
@@ -9,83 +9,118 @@ import time
 import re
 
 
-def get_vote_ct(comment_block_html, vote):
-    pattern = r"aria-label=\"(\d{1,2}) Thumbs " + vote + r"\""
-    thumb_se = re.search(pattern, comment_block_html)
-    if thumb_se:
-        return int(thumb_se.group(1))
-    return 0
+class YF_comments_analyzer:
 
-def YF_comments_fetcher(*, web_links: list):
-    xp_elems = json_reader(file_name="xp_elems.json")
-    soup_elems = json_reader(file_name="soup_elems.json")
+    def __init__(self):
+        self.xp_elems = json_reader(file_name="xp_elems.json")
+        self.soup_elems = json_reader(file_name="soup_elems.json")
 
-    options = webdriver.ChromeOptions()
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--ignore-ssl-errors')
-    options.add_argument('headless')
-    options.add_argument("--log-level=3")
-    for web_link in web_links:
-        driver = webdriver.Chrome(chrome_options=options)
-        driver.get(web_link)
+    def set_up_driver_options(self):
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument('--ignore-certificate-errors')
+        self.options.add_argument('--ignore-ssl-errors')
+        self.options.add_argument('headless')
+        self.options.add_argument("--log-level=3")
 
-        try:
-            WebDriverWait(driver, 20).until(
-                EC.presence_of_element_located((By.XPATH, xp_elems["top_react"]))
+    def driver_get_link(self, web_link):
+        self.driver = webdriver.Chrome(chrome_options=self.options)
+        self.driver.get(web_link)
+
+    def driver_select_newest(self):
+        WebDriverWait(self.driver, 30).until(
+            EC.presence_of_element_located((By.XPATH, self.xp_elems["top_react"]))
+        )
+        self.driver.find_element_by_xpath(self.xp_elems["top_react"]).click()
+        self.driver.find_element_by_xpath(self.xp_elems["newest"]).click()
+        time.sleep(10)
+
+    def driver_load_all(self):
+        while not self.driver.find_elements_by_xpath(self.xp_elems["old_time_stamp"]):
+            WebDriverWait(self.driver, 30).until(
+                EC.presence_of_element_located((By.XPATH, self.xp_elems["show_more"]))
             )
+            self.driver.find_element_by_xpath(self.xp_elems["show_more"]).click()
+            print("Clicking [Show More] to load more comments")
+        time.sleep(5)
 
+    def print_stock_info(self):
+        print(f'\nTitle: {self.driver.find_element_by_xpath(self.xp_elems["title"]).text}')
+        print(f'Index: {self.driver.find_element_by_xpath(self.xp_elems["index"]).text}')
+        print(f'Movement: {self.driver.find_element_by_xpath(self.xp_elems["movement"]).text}')
+
+    def get_comment_block_list(self):
+        print("\nComments within past 24 hours\n")
+        comment_list_ele = self.driver.find_element_by_xpath(self.xp_elems["comment_list"])
+        comment_list_html = comment_list_ele.get_attribute('innerHTML')
+        comment_list_soup = BeautifulSoup(comment_list_html, 'html.parser')
+        self.comment_block_list = comment_list_soup.find_all("li", self.soup_elems["comment_block"])
+
+    def get_comment_info(self):
+        for comment_block in self.comment_block_list:
+            user = comment_block.find("button", self.soup_elems["user_tag"])
+            time_stamp = comment_block.find("span", self.soup_elems["time_stamp"]).find("span")
+            comment_texts = comment_block.find_all("div", self.soup_elems["comment_text"])
+
+            thumb_up_ct = self.get_vote_ct(str(comment_block), vote="Up")
+            thumb_down_ct = self.get_vote_ct(str(comment_block),vote="Down")
+
+            if re.match(r".*(second|minute|hour).*", time_stamp.text):
+                print("+"*80)
+                print(f"[{user.text}] [{time_stamp.text}] [{thumb_up_ct}-Up][{thumb_down_ct}-Down]")
+                for comment_text in comment_texts:
+                    print(comment_text.text)
+                    self.comment_text_list.append(comment_text.text)
+
+    @classmethod
+    def get_vote_ct(self, comment_block_html, vote):
+        pattern = r"aria-label=\"(\d{1,2}) Thumbs " + vote + r"\""
+        thumb_se = re.search(pattern, comment_block_html)
+        if thumb_se:
+            return int(thumb_se.group(1))
+        return 0
+
+    def draw_word_map(self):
+        from nltk.corpus import stopwords
+        from nltk.tokenize import wordpunct_tokenize
+        from wordcloud import WordCloud as wc
+        import matplotlib.pyplot as plt
+
+        comments = " ".join(self.comment_text_list)
+        stop = set(stopwords.words('english'))
+        list_of_words = [i.lower() for i in wordpunct_tokenize(comments)
+            if i.lower() not in stop and i.isalpha()]
+
+        words_block = " ".join(list_of_words)
+
+        for word in ("market", "week", "tsla", "going", "tesla", "https", "http", "stock"):
+            words_block = words_block.replace(word, "")
+
+        wc1 = wc().generate(words_block)
+        plt.imshow(wc1)
+        plt.axis("off")
+        plt.show()
+
+
+    def fetch_data(self, link):
+        self.set_up_driver_options()
+        try:
             print("="*80)
-            print(f'\nTitle: {driver.find_element_by_xpath(xp_elems["title"]).text}')
-            print(f'Index: {driver.find_element_by_xpath(xp_elems["index"]).text}')
-            print(f'Movement: {driver.find_element_by_xpath(xp_elems["movement"]).text}')
-            print('\nFetching Newest Comments...\n')
-
-            driver.find_element_by_xpath(xp_elems["top_react"]).click()
-            driver.find_element_by_xpath(xp_elems["newest"]).click()
-            for i in range(3):
-                time.sleep(3)
-                print("Clicking [Show More]")
-                driver.find_element_by_xpath(xp_elems["show_more"]).click()
-
-            # wait for 15 seconds before parsing through the file
-            time.sleep(10)
-            print("\nComments within past 24 hours\n")
-
-            # Now trying - get comment block element instead
-            # ------------------------------------------------------------------
-            comment_list_ele = driver.find_element_by_xpath(xp_elems["comment_list"])
-            comment_list_html = comment_list_ele.get_attribute('innerHTML')
-
-            comment_list_soup = BeautifulSoup(comment_list_html, 'html.parser')
-            comment_block_list = comment_list_soup.find_all("li", soup_elems["comment_block"])
-
-            comment_text_list = []
-
-            for comment_block in comment_block_list:
-                user = comment_block.find("button", soup_elems["user_tag"])
-                time_stamp = comment_block.find("span", soup_elems["time_stamp"]).find("span")
-                comment_texts = comment_block.find_all("div", soup_elems["comment_text"])
-
-                thumb_up_ct = get_vote_ct(str(comment_block), "Up")
-                thumb_down_ct = get_vote_ct(str(comment_block), "Down")
-
-                if re.match(r".*(second|minute|hour).*", time_stamp.text):
-                    print("+"*80)
-                    print(f"[{user.text}] [{time_stamp.text}] [{thumb_up_ct}-Up][{thumb_down_ct}-Down]")
-                    for comment_text in comment_texts:
-                        print(comment_text.text)
-                        comment_text_list.append(comment_text.text)
-            # ------------------------------------------------------------------
-
-
+            self.driver_get_link(link)
+            self.driver_select_newest()
+            self.driver_load_all()
+            self.print_stock_info()
+            self.get_comment_block_list()
+            self.comment_text_list = []
+            self.get_comment_info()
         finally:
             print("="*80)
-            driver.quit()
-            return comment_text_list
+            self.driver.quit()
+
 
 if __name__ == '__main__':
     # download_driver()
     web_links = json_reader(file_name="web_links.json")
-    comments = YF_comments_fetcher(web_links=web_links)
-    with open("test_input.txt", "w") as write_file:
-        write_file.write(" ".join(comments))
+    analyzer = YF_comments_analyzer()
+    for link in web_links:
+        analyzer.fetch_data(link)
+        analyzer.draw_word_map()
