23b0dc6a69a0ad44ea30a0a5a2404a1c3b5224ef
Zhiheng Dong <zhiose91@gmail.com>
2020-04-09 15:15:38 -0400

    Remove special encoding in the comment text

--- src/misc.py
+++ src/misc.py
@@ -1,5 +1,9 @@
 import json
+import sys
 
 def json_reader(file_name):
     with open(file_name, "r") as file:
-        return json.load(file) 
+        return json.load(file)
+
+def sp_translate(text):
+    return text.encode('ascii', 'ignore').decode('ascii')
