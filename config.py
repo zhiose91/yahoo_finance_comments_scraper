a990b9b5abf9820dc38b17f54398b9ce785715de
Zhiheng Dong <zhiose91@gmail.com>
2020-04-18 21:06:37 -0400

    MODDED: Updated xpath_elem lookups, deleted soup_elem lookups

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 
 CONFIG = {
-    "csv_output_folder"       :   r"Saved_comments",
-    "wordmap_output_folder"   :   r"Saved_daily_word_maps",
-    "log_output_folder"       :   r"Work_log",
+    "csv_output_folder"       :   r"/home/ec2-user/web_fetcher/Saved_comments",
+    "wordmap_output_folder"   :   r"/home/ec2-user/web_fetcher/Saved_daily_word_maps",
+    "log_output_folder"       :   r"/home/ec2-user/web_fetcher/Work_log",
     "ignore_words"            :   ["https", "http", "stock", "market", "week", "going", "people"],
 }
 
@@ -10,25 +10,28 @@ SITES = [
     ("SP500", "https://finance.yahoo.com/quote/%5EGSPC/community?p=%5EGSPC")
 ]
 
-SOUP_ELEMS = {
-  "time_stamp": {"class": "Fz(12px) C(#828c93)"},
-
-  "comment_block": {"class": "comment Pend(2px) Mt(5px) Mb(11px) P(12px)"},
-  "comment_text": {"class": "C($c-fuji-grey-l) Mb(2px) Fz(14px) Lh(20px) Pend(8px)"},
-
-  "thumb_up_block": {"class": "O(n):h O(n):a  Bgc(t) Bdc(t) M(0) P(0) Bd(n) Mend(12px)"},
-  "thumb_up_ct": {"class": "D(ib) Mstart(4px) Va(m) Fz(12px) C($c-fuji-grey-g)"}
-}
-
-
+# Yahoo Finance frequently changes class names, use 'contains' for fuzzy matches
 XP_ELEMS = {
-  "top_react":"//span[text()[contains(.,'Top Reactions')]]",
-  "newest":"//span[text()[contains(.,'Newest Reactions')]]",
-  "title":"//h1[@class='D(ib) Fz(18px)']",
-  "index":"//span[@class='Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)']",
-  "movement":"//span[contains(@class, 'Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($data')]",
+  "top_react"       :   "//span[text()[contains(.,'Top Reactions')]]",
+  "newest"          :   "//span[text()[contains(.,'Newest Reactions')]]",
 
-  "comment_list":"//ul[contains(@class, 'comments-list List(n)')]",
-  "old_time_stamp": "//li//div//div//span//span[text()[contains(.,'day')]]",
-  "show_more": "//span[text()[contains(.,'Show more')]]"
+  "old_time_stamp"  :   "//li//div//div//span//span[text()[contains(.,'day')]]",
+  "show_more"       :   "//span[text()[contains(.,'Show more')]]",
+
+  "title"           :   "//h1[@class='D(ib) Fz(18px)']",
+  "index"           :   "//span[contains(@class, 'Trsdu(0.3s) Fw(b)')]",
+  "movement"        :   "//span[contains(@class, 'Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px)')]",
+
+  "comment_list"    :   "//ul[contains(@class, 'comments-list')]",
+
+  "comment_block"   :   "//ul//li[contains(@class, 'comment')]",
+  "comment_user"    :   ".//button[contains(@aria-label, 'See reaction history')]",
+  "time_stamp"      :   ".//div//div//span[contains(@class, 'Fz(12px)')]//span",
+
+  "comment_text"    :   ".//div[contains(@class, 'Wow(bw)')]",
+  "comment_urls"    :   ".//a[contains(@href, 'http')]",
+  "comment_media"   :   ".//source",
+
+  "thumbup"         :  ".//button[contains(@aria-label, 'Thumbs Up')]",
+  "thumbdown"       :   ".//button[contains(@aria-label, 'Thumbs Down')]"
 }
