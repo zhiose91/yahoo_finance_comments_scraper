6e1171772d6f631fca89f5407acf7cea47783fa6
Zhiheng Dong <zhiose91@gmail.com>
2020-04-28 22:29:18 -0400

    MODDED: Set function parameter types

--- src/misc.py
+++ src/misc.py
@@ -1,10 +1,10 @@
 # misc
 
-def sp_translate(text):
+def sp_translate(text: str):
     return text.encode('ascii', 'ignore').decode('ascii')
 
 
-def check_n_mkdir(dir):
+def check_n_mkdir(dir: str):
     import os
     dir = dir.replace("\\", "/")
     if not os.path.exists(dir):
