13014c31424f24409bfec3110b68f18fc6c89c85
Zhiheng Dong <zhiose91@gmail.com>
2020-04-30 23:07:10 -0400

    ADDED: Json output folder

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 CONFIGS = {
     "csv_output_folder"       :   r"Saved_comments",
-    "wordmap_output_folder"   :   r"Saved_daily_word_maps",
+    "json_output_folder"      :   r"Saved_comments",
+    "word_cloud_output_folder":   r"Saved_daily_word_clouds",
     "log_output_folder"       :   r"Work_log"
 }
 
 SITES = [
-    ("SP500", "https://finance.yahoo.com/quote/%5EGSPC/community?p=%5EGSPC")
+    # ("SP500", "https://finance.yahoo.com/quote/%5EGSPC/community?p=%5EGSPC"),
+    ("Amazon", "https://finance.yahoo.com/quote/AMZN/community?p=AMZN")
 ]
