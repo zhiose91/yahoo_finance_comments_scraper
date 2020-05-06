bc14d6e7187f6555256b4818217d8d3e55d3c243
Zhiheng Dong <zhiose91@gmail.com>
2020-05-05 23:09:54 -0400

    MODDED: Link updates

--- config.py
+++ config.py
@@ -6,6 +6,11 @@ CONFIGS = {
 }
 
 SITES = [
-    # ("SP500", "https://finance.yahoo.com/quote/%5EGSPC/community?p=%5EGSPC"),
-    ("Amazon", "https://finance.yahoo.com/quote/AMZN/community?p=AMZN")
+    ("SP500", "https://finance.yahoo.com/quote/%5EGSPC/community?p=%5EGSPC"),
+    # ("DowJones", "https://finance.yahoo.com/quote/%5EDJI/community?p=%5EDJI"),
+    # ("Facebook", "https://finance.yahoo.com/quote/FB/community?p=FB"),
+    # ("Amazon", "https://finance.yahoo.com/quote/AMZN/community?p=AMZN"),
+    # ("Apple", "https://finance.yahoo.com/quote/AAPL/community?p=AAPL"),
+    # ("Netflix", "https://finance.yahoo.com/quote/NFLX/community?p=NFLX"),
+    # ("Google", "https://finance.yahoo.com/quote/GOOG/community?p=GOOG"),
 ]
