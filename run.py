081d43117f6f0e73f7a614dbae30f2879e230c38
Zhiheng Dong <zhiose91@gmail.com>
2020-05-13 00:11:04 -0400

    ADDED: Now put fetched instances to dynamodb

--- run.py
+++ run.py
@@ -12,12 +12,12 @@ CONFIGS = {
 
 SITES = [
     ("SP500", "https://finance.yahoo.com/quote/%5EGSPC/community?p=%5EGSPC"),
-    ("DowJones", "https://finance.yahoo.com/quote/%5EDJI/community?p=%5EDJI"),
-    ("Facebook", "https://finance.yahoo.com/quote/FB/community?p=FB"),
-    ("Amazon", "https://finance.yahoo.com/quote/AMZN/community?p=AMZN"),
-    ("Apple", "https://finance.yahoo.com/quote/AAPL/community?p=AAPL"),
-    ("Netflix", "https://finance.yahoo.com/quote/NFLX/community?p=NFLX"),
-    ("Google", "https://finance.yahoo.com/quote/GOOG/community?p=GOOG"),
+    # ("DowJones", "https://finance.yahoo.com/quote/%5EDJI/community?p=%5EDJI"),
+    # ("Facebook", "https://finance.yahoo.com/quote/FB/community?p=FB"),
+    # ("Amazon", "https://finance.yahoo.com/quote/AMZN/community?p=AMZN"),
+    # ("Apple", "https://finance.yahoo.com/quote/AAPL/community?p=AAPL"),
+    # ("Netflix", "https://finance.yahoo.com/quote/NFLX/community?p=NFLX"),
+    # ("Google", "https://finance.yahoo.com/quote/GOOG/community?p=GOOG"),
 ]
 
 
@@ -27,3 +27,22 @@ for instance_name, link in SITES:
 scraper.dump_instance_json()
 
 loader = Comment_loader()
+loader.connect_to_db("Test", "Test")
+# loader.connect_to_db()
+loader.set_table("Yahoo_fin_comment")
+
+for ins_title, ins_info in scraper.fetched_instances.items():
+    scraper.log(
+        f"Dynamodb-Ops: Inserting pKey:[{ins_title}] sKey:[{ins_info['fetched_date']}]",
+        mode="main"
+    )
+
+    loader.table.put_item(
+        Item={
+            "instance_name" : ins_info["ins_title"],
+            "fetched_date"  : ins_info["fetched_date"],
+            "fetch_info"    : ins_info
+        }
+    )
+
+    scraper.log(f"Dynamodb-Ops: Valid insertion", mode="sub")
