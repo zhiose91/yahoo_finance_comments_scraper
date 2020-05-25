bca0066814526a3124c1118aa443b98ef725d4dd
Zhiheng Dong <zhiose91@gmail.com>
2020-05-24 22:48:38 -0400

    MOD: Removed unused lines in word cloud class

--- src/misc.py
+++ src/misc.py
@@ -28,24 +28,21 @@ def check_n_mkdir(dir: str):
 class Comment_word_cloud_generator:
 
     def __init__(self):
-        pass
+        self.__word_cloud_output_folder = check_n_mkdir("tmp")
 
-    def draw_word_cloud(self, wc_show=False):
+    def draw_word_cloud(self, words_chunck:str, wc_show=False):
         """Generating word cloud using the stored comments"""
         from wordcloud import WordCloud as wc
         import matplotlib.pyplot as plt
 
-        self.log(f'Generating word cloud: [{self.ins_title}]')
-
         wc_graph = wc(
             max_words=200, background_color="white",
-            collocations = False).generate(self.words_chunck)
+            collocations = False).generate(words_chunck)
 
         self.wc_plot = plt
 
         self.wc_plot.figure(figsize=(12,8))
         self.wc_plot.imshow(wc_graph, interpolation="bilinear")
-        self.wc_plot.title(f"[{self.ins_title}]\n[{self.ins_index}]  [{self.movement}]")
         self.wc_plot.axis("off")
         self.wc_plot.tight_layout(pad=1)
 
@@ -55,13 +52,4 @@ class Comment_word_cloud_generator:
 
     def save_word_cloud(self, file_name: str=""):
         """Save word cloud locally"""
-        if file_name:
-            self.wc_file_name = file_name
-        else:
-            self.wc_file_name = os.path.join(
-                check_n_mkdir(self.__word_cloud_output_folder),
-                f"{self.ins_title} - {self.log_date_str}.JPG"
-            )
-
-        self.log(f'Saved word cloud as: {self.wc_file_name}', mode="sub")
-        self.wc_plot.savefig(self.wc_file_name)
+        self.wc_plot.savefig(file_name)
